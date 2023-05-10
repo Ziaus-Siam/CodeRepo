2122eb353af30e8801abc5f4b7b4b0366387776f
Muhammad Ziaus Siam <ziaus.siam@gmail.com>
2023-05-10 23:35:52 +0600

    Implemented Hello World Console Application

---
+++ Beginner/1.HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+/**********************************************************************
+ * file   main.cpp
+ * brief  Hello World Console App.
+ * author Muhammad Ziaus Siam
+ * date   May 2023
+ *********************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
